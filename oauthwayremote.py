f28aa7dd9f21f22d76b3b996387e207adaff4b68
Aneesh P U <aneeshpu@gmail.com>
2011-02-14 16:20:24 +0530

    nothing works still, I get an invalid signature error from google

--- oauthwayremote.py
+++ oauthwayremote.py
@@ -18,17 +18,28 @@ class OAuthWayRemote(webapp.RequestHandler):
     self.response.out.write("signature base string in request is {0}".format(signature_base_string))
     self.response.out.write("<br/>")
     hmac_sha1 = hmac.new ('8ZIsnRdNjD6eBtQ1oTjlnosZ',signature_base_string, hashlib.sha1)
-    signature = hmac_sha1.digest() 
+    signature = hmac_sha1.hexdigest() 
+    self.response.out.write("<hr/>")
+    self.response.out.write(signature)
     self._get_request_token(signature)
 
   def _get_request_token(self, signature):
     https_connection = httplib.HTTPSConnection("www.google.com")
-    https_connection.request('GET',"/accounts/OAuthGetRequestToken?oauth_callback=http://localhost/WAYRemote&oauth_consumer_key=wayremote.appspot.com&oauth_nonce="+self._oauth_nonce+"&oauth_signature_method=HMAC-SHA1&oauth_signature="+signature+"&oauth_timestamp="+self._oauth_timestamp+"&scope=https://www.googleapis.com/auth/latitude")
+    oauth_headers = self._make_oauth_headers(signature)
+    https_connection.request('GET',"/accounts/OAuthGetRequestToken?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Flatitude",{},headers=oauth_headers)
     http_response = https_connection.getresponse()
     self.response.headers['Content-Type'] = 'text/html'
     self.response.out.write(http_response.status)
     self.response.out.write("<br/>")
     self.response.out.write(http_response.read())
+    
+
+  def _make_oauth_headers(self,signature):
+    headers = {'Authorization': 'OAuth oauth_callback="http%3A%2F%2Flocalhost%2FWAYRemote",oauth_consumer_key="wayremote.appspot.com",oauth_nonce="{0}",oauth_signature_method="HMAC-SHA1",oauth_signature="{1}",oauth_timestamp="{2}"'.format(self._oauth_nonce,signature,self._oauth_timestamp)}
+    self.response.out.write("<hr/>")
+    self.response.out.write(headers)
+    self.response.out.write("<br/>")
+    return headers
     
 
   def signature_base_string(self, http_method,request_baseurl, request_parameters):
@@ -45,7 +56,7 @@ class OAuthWayRemote(webapp.RequestHandler):
     self._oauth_timestamp = str(int(time.time()))
     oauth_timestamp = ("oauth_timestamp", self._oauth_timestamp)
     google_scope = ("scope", "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Flatitude")
-    oauth_callback = ("oauth_callback", "http%3A%2F%2Flocalhost%2FWAYRemote%2F")
+    oauth_callback = ("oauth_callback", "http%3A%2F%2Flocalhost%2FWAYRemote")
     req_params = [oauth_consumer_key, oauth_nonce, oauth_signature_method, oauth_timestamp, google_scope,oauth_callback]
     req_params.sort()
     return req_params
